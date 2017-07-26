5190fc28fdffa23eb1b68efb573296568ec6e889
Ethan Ward <ethan.ward@mycroft.ai>
2017-07-26 11:38:06 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 python3-tk
 cd pythonlib
-sudo pip install -r requirements.txt
+sudo pip3 install -r requirements.txt
 
