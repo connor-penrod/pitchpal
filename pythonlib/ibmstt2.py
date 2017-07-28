b6cf687c3a34709162e0055a4900e139a3211ee0
general-shift <cfpvf9@mail.missouri.edu>
2017-07-27 19:58:21 -0500

    Added better logging

--- pythonlib/ibmstt2.py
+++ pythonlib/ibmstt2.py
@@ -1,3 +1,4 @@
+import logging, traceback
 from websocket import create_connection
 from watson_developer_cloud import AuthorizationV1, SpeechToTextV1
 import json
@@ -12,9 +13,22 @@ try:
     USERNAME = parser.get('STT_settings', 'username')
     PASSWORD = parser.get('STT_settings', 'password')
 except Exception as e:
-    print(str(e))
-    print("Could not load IBM STT configuration settings. Cannot connect websocket. Disconnecting...")
+    logging.error(str(e))
+    logging.warning("Could not load IBM STT configuration settings. Cannot connect websocket. Disconnecting...")
     sys.exit()
+
+logging.basicConfig(
+    filename="stt_debug.log",
+    level=1,
+    format="*****\n%(asctime)s||%(levelname)s||line %(lineno)d||%(funcName)s: %(message)s",
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+    )
+logging.info("---------NEW DEBUG SESSION--------")
+
+def logExecutionInfo():
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+    logging.error(''.join('!! ' + line for line in lines))
 
 import pyaudio
 
@@ -37,27 +51,28 @@ def resetConnection():
     global t3
     global isRestarting
     
-    print("Restarting WS...")
+    logging.info("Restarting WS...")
     
     ws.close()
     
     ws = None
-    print("\tClosed previous websocket...")
+    logging.info("\tClosed previous websocket...")
     
     try:
         ws = create_connection('wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize?watson-token=' +
           token + '&model=en-US_BroadbandModel')
     except Exception as e:
-        print("Creating WS failed, error: " + str(e))
+        logging.error("Creating WS failed, error: " + str(e))
+        logExecutionInfo()
         return
     
-    print("\tEstablished WS connection...")
+    logging.info("\tEstablished WS connection...")
     
     ws.send('{"action":"start","content-type":"audio/l16;rate=16000","interim_results":true,"inactivity_timeout":300}')
     
-    print("\tSent initiation request to IBM")
+    logging.info("\tSent initiation request to IBM")
     
-    print("Websocket recreated, starting Mic stream again...")
+    logging.info("Websocket recreated, starting Mic stream again...")
 
     isRestarting = False
     getMicData(ws)
@@ -74,7 +89,7 @@ def getMicData(ws):
     stream = audio.open(format=FORMAT, channels=CHANNELS,
                     rate=RATE, input=True, #input_device_index=inputDeviceIndex,
                     frames_per_buffer=CHUNK)
-    print("recording...")
+    logging.info("Mic stream initiated, recording...")
     frames = []
     totalData = b''
     bytesSent = 0
@@ -82,7 +97,7 @@ def getMicData(ws):
         try:
             data = stream.read(CHUNK)
         except Exception as e:
-            print("Stream reading failed, error: " + str(e))
+            logging.error("Stream reading failed, error: " + str(e))
 
         totalData += data
 
@@ -91,9 +106,10 @@ def getMicData(ws):
                 dataChunk = totalData[0:CHUNKSIZE]
                 ws.send(dataChunk, opcode=0x2)
             except Exception as e:
-                print("Sending failed, error: " + str(e))
+                logging.error("Sending failed, error: " + str(e))
+                logExecutionInfo()
                 resetConnection()
-                print("Closing mic data stream")
+                logging.info("Closing mic data stream")
                 stream.stop_stream()
                 stream.close()
                 audio.terminate()
@@ -104,7 +120,7 @@ def getMicData(ws):
         #print(str(bytesSent/1000/1000) + " megabytes")
         
 
-    print("finished recording")
+    logging.info("Mic stream has finished recording, closing stream, terminating audio...")
      
      
     # stop Recording
@@ -130,13 +146,18 @@ def receiveAudio():
     while True:
         try:
             if(check_pid(int(sys.argv[2])) is False):
-                print("PitchPal termination detected, closing stream and websocket...")
+                logging.warning("PitchPal termination detected, closing stream and websocket...")
                 ws.close()
                 os.kill(os.getpid(), 9)
+        except Exception as e:
+            logging.error("Receiving failed, error: " + str(e))
+            logExecutionInfo()
+
+        try:
             result = json.loads(ws.recv())
         except Exception as e:
-            print("Receiving failed, error: " + str(e))
-            
+            pass
+
         print("----------")
         if("results" in result):
             print(result["results"][0]["alternatives"][0]["transcript"])
